2348c7893d6f183d3e31250e362c8abc522f1a5e
BendalfM <bensamgon.ru@gmail.com>
2026-09-20 18:23:35 +0400

    new hello world file

--- hello-world/src/main/java/com/example/HelloWorld.java
+++ hello-world/src/main/java/com/example/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! v4.0");
+        System.out.println("Hello World! v5.0");
     }
 }
